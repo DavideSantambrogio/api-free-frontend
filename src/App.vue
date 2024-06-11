bb254152042cc052bfc7a5affb565bf068258cd0
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-06-11 10:24:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+<h1>hello world</h1>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css"; 
+
+</style>
