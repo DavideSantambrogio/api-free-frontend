798e3bcc7fbd59db36fcaae340e6d6b502736d92
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-06-11 14:16:10 +0200

    add:navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <script>
+import Navbar from './components/navbar.vue'
+import vista from './components/vista.vue';
+
+
 export default {
+    components: {
+        Navbar,
+        vista
+    }
 
 }
 </script>
 
 <template>
-<h1>hello world</h1>
+<Navbar />
+<div>
+    <vista />
+</div>
+
 </template>
 
 <style lang="scss">
 @use "./style/general.scss";
 @import "@fortawesome/fontawesome-free/css/all.css"; 
+*{
+    // border: 1px solid red;
+}
 
 </style>
